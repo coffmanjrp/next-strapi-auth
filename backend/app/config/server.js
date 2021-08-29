5952bf19211894f27799dd5263f2a2f056b53ccf
coffmanjrp <coffmanjrp@gmail.com>
2021-08-29 04:09:02 -0700

    Setup strapi backend

---
+++ backend/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ea63aead85772e636d33b3bf42cba46c'),
+    },
+  },
+});
---
+++ backend/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8e281c40-4550-46b5-bf91-202f088cc5c1'
+};
